b4f52aa907deae67ec7b090005b11849ea205123
antharia <artenischloria@gmail.com>
2020-09-19 18:58:06 +0200

    new config files

--- backup_dotfiles.sh
+++ backup_dotfiles.sh
@@ -7,7 +7,10 @@ cp -r ~/.config/i3status ./config/i3status
 cp -r ~/.config/rofi ./config/rofi
 cp -r ~/.config/termite ./config/termite
 cp -r ~/.config/wal ./config/wal
+cp ~/.tmux.conf ./tmux.conf
+cp ~/.bashrc ./bashrc
+
+
+cp -r ~/Images/wallpapers Images/wallpapers
 git add -A
 git commit -m "new config files"
-git log
-    
