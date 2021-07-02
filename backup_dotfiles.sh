89aaa4be8853cfc7c3c38fed7eedcf5a3c5a2711
Antharia <artenischloria@gmail.com>
2021-07-02 10:40:37 +0200

    new config files

--- backup_dotfiles.sh
+++ backup_dotfiles.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-cp -r ~/.i3 ~/Repos/dotfiles/.i3
-cp ~/.vimrc ~/Repos/dotfiles/.vimrc
-cp ~/.Xresources ~/Repos/dotfiles/.Xresources
-cp ~/.zshrc ~/Repos/dotfiles/.zshrc
-cp -r ~/.config/i3status ./.config/i3status
-cp -r ~/.config/rofi ./.config/rofi
-cp -r ~/.config/termite ~/Repos/dotfiles/.config/termite
-cp -r ~/.config/wal ~/Repos/dotfiles/.config/wal
-cp ~/.tmux.conf ~/Repos/dotfiles/.tmux.conf
-cp ~/.bashrc ~/Repos/dotfiles/.bashrc
-cp -r ~/.config/zathura ~/Repos/dotfiles/.config/zathura
-cp -r ~/.config/rclone ~/Repos/dotfiles/.config/rclone
+cp -r ~/.i3 ~/Repos/dotfiles/i3
+cp ~/.vimrc ~/Repos/dotfiles/vimrc
+cp ~/.Xresources ~/Repos/dotfiles/Xresources
+cp ~/.zshrc ~/Repos/dotfiles/zshrc
+cp -r ~/.config/i3status ./Repos/dotfiles/i3status
+cp -r ~/.config/rofi ~/Repos/dotfiles/rofi
+cp -r ~/.config/termite ~/Repos/dotfiles/termite
+cp -r ~/.config/wal ~/Repos/dotfiles/wal
+cp ~/.tmux.conf ~/Repos/dotfiles/tmux.conf
+cp ~/.bashrc ~/Repos/dotfiles/bashrc
+cp -r ~/.config/zathura ~/Repos/dotfiles/zathura
+cp -r ~/.config/rclone ~/Repos/dotfiles/rclone
 cp -r ~/Images/wallpapers ~/Repos/dotfiles/Images/wallpapers
 cp ~/setup_manjaro.sh ~/Repos/dotfiles/setup_manjaro.sh
 git add -A
--- restore_dotfiles.sh
+++ restore_dotfiles.sh
@@ -3,10 +3,13 @@ cp -r ~/Repos/dotfiles/i3 ~/.i3
 cp ~/Repos/dotfiles/vimrc ~/.vimrc
 cp ~/Repos/dotfiles/Xresources ~/.Xresources
 cp ~/Repos/dotfiles/zshrc ~/.zshrc
-cp -r ~/Repos/dotfiles/.config/i3status ~/.config/i3status 
-cp -r ~/Repos/dotfiles/.config/rofi ~/.config/rofi
-cp -r ~/Repos/dotfiles/.config/termite ~/.config/termite
-cp ~/Repos/dotfiles/bashrc ~/bashrc
+cp -r ~/Repos/dotfiles/i3status ~/.config/i3status 
+cp -r ~/Repos/dotfiles/rofi ~/.config/rofi
+cp -r ~/Repos/dotfiles/termite ~/.config/termite
+cp -r ~/Repos/dotfiles/wal ~/.config/wal
 cp ~/Repos/dotfiles/tmux.conf ~/.tmux.conf
-cp ~/Repos/dotfiles/xinitrc ~/.xinitrc
-cp -r ~/Repos/dotfiles/config/zathura ~/.config/
+cp ~/Repos/dotfiles/bashrc ~/bashrc
+cp -r ~/Repos/dotfiles/zathura ~/.config/zathura
+cp -r ~/Repos/dotfiles/rclone ~/.config/rclone
+cp ~/Repos/dotfiles/setup_manjaro.sh ~/setup_manjaro.sh
+echo "Dotfiles restoration done."
